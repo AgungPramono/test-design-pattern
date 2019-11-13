fdc7853b53c68bd45abb4108115e61e0d0aa94bc
agung <agung@bits-soft.com>
2019-11-13 16:08:18 +0700

    rename package

--- src/main/java/com/agung/dp/builder/HouseBuilderDemo.java
+++ src/main/java/com/agung/dp/builder/HouseBuilderDemo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.agung.dp.bp;
+package com.agung.dp.builder;
 
 import lombok.Data;
 import lombok.Getter;
